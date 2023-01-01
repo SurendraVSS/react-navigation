a6961142b4f4453be67d2a9952320c8f64515cee
SurendraVSS <106951672+SurendraVSS@users.noreply.github.com>
2023-01-01 15:26:35 +0530

    model in react native

--- App.js
+++ App.js
@@ -1,10 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Modal, Button, ScrollView } from 'react-native';
+import { useState } from 'react'
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(true);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    {
+        modalVisible ? null : <Button
+          title='Show Pop Up'
+          onPress={() => setModalVisible(true)}
+        />
+    }
+   
+      <Modal
+        visible={modalVisible}
+        
+        transparent={true}
+      >
+      <View
+            style={styles.modal}
+          >
+      <ScrollView>
+          
+            <Button
+              title="close"
+              onPress={() => setModalVisible(false)}
+            />
+            <Text>hello</Text>
+            <Text>hello</Text>
+            <Text>hello</Text>
+            <Text>hello</Text>
+            <Text>hello</Text>
+            <Text>hello</Text>
+            <Text>hello</Text>
+            <Text>hello</Text>
+            <Text>hello</Text>
+            <Text>hello</Text>
+            <Text>hello</Text>
+            <Text>hello</Text>
+            <Text>hello</Text>
+            <Text>hello</Text>
+            <Text>hello</Text>
+            <Text>hello</Text>
+         
+      </ScrollView>
+        </View>
+    
+      </Modal>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +56,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  modal:{
+    width: '80%',
+    
+    height: '20%',
+    backgroundColor: 'white',
+    justifyContent:'center',
+    alignItems: 'center',
+    marginTop:150,
+    marginLeft:30
+  }
 });
